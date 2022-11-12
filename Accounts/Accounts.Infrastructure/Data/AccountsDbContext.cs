21e82e987c506e7397aa475e33730351e0a71adb
julioschuambach <julioschuambach.dev@gmail.com>
2022-11-12 10:23:09 -0300

    Criação e configuração inicial da classe responsável pelo acesso à dados

---
+++ Accounts/Accounts.Infrastructure/Data/AccountsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Accounts.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Accounts.Infrastructure.Data;
+
+public class AccountsDbContext : DbContext
+{
+    private readonly string _connectionString = "Server = localhost, 1433; Database = AccountsDb; User ID = sa; Password = 1q2w3e4r@#$;";
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_connectionString);
+}
