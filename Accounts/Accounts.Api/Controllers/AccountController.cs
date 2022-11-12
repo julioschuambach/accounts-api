115a6c9c36cf7d45b3812b28725b01470e2c8f9b
julioschuambach <julioschuambach.dev@gmail.com>
2022-11-12 15:53:30 -0300

    Remoção de referências desnecessárias após refatoração

--- Accounts/Accounts.Api/Controllers/AccountController.cs
+++ Accounts/Accounts.Api/Controllers/AccountController.cs
@@ -1,9 +1,7 @@
 ﻿using Accounts.Api.ViewModels;
 using Accounts.Domain.Entities;
-using Accounts.Infrastructure.Data;
 using Accounts.Infrastructure.Data.Repositories.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Accounts.Api.Controllers;
 
