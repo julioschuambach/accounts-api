234ff579e021d133fd2566b9312df31acc67e5f8
julioschuambach <julioschuambach.dev@gmail.com>
2022-11-12 15:49:35 -0300

    Implementação do Repository Pattern para abstrair o acesso à dados do controlador

--- Accounts/Accounts.Api/Program.cs
+++ Accounts/Accounts.Api/Program.cs
@@ -1,9 +1,12 @@
 using Accounts.Infrastructure.Data;
+using Accounts.Infrastructure.Data.Repositories;
+using Accounts.Infrastructure.Data.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AccountsDbContext>();
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
 
 var app = builder.Build();
 app.MapControllers();
---
+++ Accounts/Accounts.Infrastructure/Data/Repositories/AccountRepository.cs
@@ -0,0 +1,31 @@
+﻿using Accounts.Domain.Entities;
+using Accounts.Infrastructure.Data.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Accounts.Infrastructure.Data.Repositories;
+
+public class AccountRepository : IAccountRepository
+{
+    private readonly AccountsDbContext _context;
+
+    public AccountRepository(AccountsDbContext context)
+        => _context = context;
+
+    public Account CreateAccount(Account account)
+    {
+        _context.Accounts.Add(account);
+        _context.SaveChanges();
+
+        return account;
+    }
+
+    public Account GetAccountByEmail(string email)
+    {
+        var account = _context.Accounts
+                                .AsNoTracking()
+                                .Include(x => x.Roles)
+                                .FirstOrDefault(x => x.Email == email);
+
+        return account;
+    }
+}
