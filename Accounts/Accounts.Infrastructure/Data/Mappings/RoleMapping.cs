ef0d9da9a522ffb1f7fc0882facc423e0bfd451c
julioschuambach <julioschuambach.dev@gmail.com>
2023-09-24 00:40:17 -0300

    feat: created mapping to Role entity

---
+++ Accounts/Accounts.Infrastructure/Data/Mappings/RoleMapping.cs
@@ -0,0 +1,35 @@
+﻿using Accounts.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Accounts.Infrastructure.Data.Mappings;
+
+public class RoleMapping : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.ToTable("Roles");
+
+        builder.HasKey(x => x.Id)
+            .HasName("PK_Roles_Id");
+
+        builder.Property(x => x.Id)
+            .HasColumnName("Id")
+            .HasColumnType("UNIQUEIDENTIFIER")
+            .IsRequired();
+
+        builder.Property(x => x.Name)
+            .HasColumnName("Name")
+            .HasColumnType("VARCHAR")
+            .HasMaxLength(25)
+            .IsRequired();
+
+        builder.HasMany(x => x.Accounts);
+
+        builder.HasIndex(x => x.Id, "IX_Roles_Id")
+            .IsUnique();
+
+        builder.HasIndex(x => x.Name, "IX_Roles_Name")
+            .IsUnique();
+    }
+}
