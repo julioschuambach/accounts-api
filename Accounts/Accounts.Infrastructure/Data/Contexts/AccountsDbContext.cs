d32dbfa3896b571a01aa11e70e3067e87bc0633f
julioschuambach <julioschuambach.dev@gmail.com>
2023-09-24 00:29:44 -0300

    feat: created context and access to the database

---
+++ Accounts/Accounts.Infrastructure/Data/Contexts/AccountsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Accounts.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Accounts.Infrastructure.Data;
+
+public class AccountsDbContext : DbContext
+{
+    private readonly IConfiguration _configuration;
+    public DbSet<Account> Accounts { get; set; }
+    public DbSet<Role> Roles { get; set; }
+
+    public AccountsDbContext(IConfiguration configuration)
+        => _configuration = configuration;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("Default"));
+}
