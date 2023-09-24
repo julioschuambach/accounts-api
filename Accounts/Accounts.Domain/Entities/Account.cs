4bf9e8b9c8fb7dc4f11d1705a04246b5e9867479
julioschuambach <julioschuambach.dev@gmail.com>
2023-09-24 00:24:33 -0300

    feat: created Account and Role entities

---
+++ Accounts/Accounts.Domain/Entities/Account.cs
@@ -0,0 +1,23 @@
+﻿using Highscores.Domain.Entities.Base;
+
+namespace Accounts.Domain.Entities;
+
+public class Account : EntityBase
+{
+    public string Username { get; private set; }
+    public string Email { get; private set; }
+    public string Password { get; private set; }
+    public DateTime CreatedDate { get; private set; }
+    public DateTime LastUpdatedDate { get; private set; }
+    public IList<Role> Roles { get; private set; }
+
+    public Account(string username, string email, string password)
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+        CreatedDate = DateTime.Now;
+        LastUpdatedDate = DateTime.Now;
+        Roles = new List<Role>();
+    }
+}
